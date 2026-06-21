d1edce9c4d0f1e83aa113975b684b758d06570a8
Roman <romavin80@gmail.com>
2026-06-21 19:59:54 +0300

    Сохранение текущих изменений в utils.py

--- utils.py
+++ utils.py
@@ -1,2 +1,14 @@
 def add(a,b):
+    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        return a+b
     return a+b
+    raise TypeError
+
+def product(a,b):
+    return a*b
+
+def subtract(a,b):
+    return a-b
+
+def divide(a,b):
+    return a/b
