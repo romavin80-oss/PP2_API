e1f9fd8ce5e2fbdbb37476a71ce885949d063b88
Roman <romavin80@gmail.com>
2026-06-21 19:36:37 +0300

    написана функция: divide

--- utils.py
+++ utils.py
@@ -1,2 +1,14 @@
 def add(a,b):
-    return a+b
+    if isinstance(a,(int, float)) and isinstance(b,(int, float)):
+        return a+b
+    return TypeError
+
+def product(a,b):
+        return a*b
+
+def subtract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a**b
+
