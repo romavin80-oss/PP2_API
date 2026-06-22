0bb2d79a5913434df158cfbf6a21abb9be281dee
Roman <romavin80@gmail.com>
2026-06-22 19:30:21 +0300

    - Добавлен CRUD для модели Sections; - Дщбавлены urls для CRUD Sections - В базовые urls добавлены  urls для Sections

---
+++ sections/urls.py
@@ -0,0 +1,28 @@
+from os import path as p
+
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from sections.apps import SectionsConfig
+from sections.views import SectionListApiView, SectionCreateApiView, SectionRetrieveAPIView, SectionUpdateAPIView, SectionDestroyAPIView
+
+app_name = SectionsConfig.name
+
+router = DefaultRouter()
+
+section = 'section/'
+content = 'content/'
+question = 'question/'
+create = 'create/'
+update = 'update/'
+delete = 'delete/'
+int_pk = '<int:pk>/'
+
+urlpatterns = [
+    # section urlpatterns
+    path(p.join(section), SectionListApiView.as_view(), name='section_list'),
+    path(p.join(section, create), SectionCreateApiView.as_view(), name='section_create'),
+    path(p.join(section, int_pk), SectionRetrieveAPIView.as_view(), name='section_detail'),
+    path(p.join(section, int_pk, update), SectionUpdateAPIView.as_view(), name='section_update'),
+    path(p.join(section, int_pk, delete), SectionDestroyAPIView.as_view(), name='section_delete'),
+] + router.urls
