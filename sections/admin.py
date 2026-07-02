9bcf46c0cffbfb86700e710dcefd5cd5d86854b3
Roman <romavin80@gmail.com>
2026-07-02 13:30:26 +0300

    WIP: обновление админки sections

--- sections/admin.py
+++ sections/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from sections.models import Section, Content
+
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title',)
+    list_filter = ('title',)
+    ordering = ('id',)
+    search_fields = ('title',)
+
+
+@admin.register(Content)
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'section', 'title')
+    list_filter = ('section',)
+    ordering = ('id', 'section',)
+    search_fields = ('title',)
